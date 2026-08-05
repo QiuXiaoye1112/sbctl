e36040f4f0e776369f7fb83cf6174e990a92aeb5
QiuXiaoye1112 <ethantechie1231@hotmail.com>
2026-08-05 19:23:46 +0800

    fix: keep OpenRC runtime firewall-free

--- lib/engine.sh
+++ lib/engine.sh
@@ -132,7 +132,7 @@ command_background="yes"
 pidfile="/run/${SERVICE_NAME}.pid"
 output_log="/var/log/sing-box.log"
 error_log="/var/log/sing-box.log"
-depend() { need net; after firewall; }
+depend() { need net; }
 EOF_RC
       chmod 755 "${OPENRC_INIT_DIR}/$SERVICE_NAME"
       ;;
