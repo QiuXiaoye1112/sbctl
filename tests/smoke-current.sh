198dab7b789819e0f39c9b5a293c243f9a037a37
QiuXiaoye1112 <ethantechie1231@hotmail.com>
2026-08-05 19:40:52 +0800

    test: align SOCKS share assertion with authenticated URI

--- tests/smoke-current.sh
+++ tests/smoke-current.sh
@@ -9,6 +9,8 @@ awk '
   /^run_generated_case mixed / { skip=1; next }
   skip && /^HY=/ { skip=0 }
   !skip { print }
-' "$ROOT/tests/smoke.sh" >"$TMP"
+' "$ROOT/tests/smoke.sh" \
+  | sed "s#'socks://127\\.0\\.0\\.1:24449'#'@127.0.0.1:24449'#" \
+  >"$TMP"
 
 SBCTL_TESTING=1 bash "$TMP"
